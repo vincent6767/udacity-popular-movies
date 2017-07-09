53d509d251e0c6064a131d67a48e130e7da24c23
Vincent <vincentbiasa6767@gmail.com>
2017-07-09 16:24:07 +0700

    Support grid layout on landscape mode.

--- app/src/main/java/com/example/android/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovies/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -60,11 +61,12 @@ public class MainActivity extends AppCompatActivity implements MovieAdapter.Movi
         initService();
         // Initialize movies list.
         fetchMoviesData(mCurrentOption, DATA_OPERATION.SET);
+        Log.d("onCreate", "True");
     }
     private void initViews() {
         rvMoviesList = (RecyclerView) findViewById(R.id.rv_movies);
         rvMoviesList.setHasFixedSize(true);
-        int mNumberOfCols = 2;
+        int mNumberOfCols = getResources().getInteger(R.integer.gallery_columns);
         GridLayoutManager layoutManager = new GridLayoutManager(this, mNumberOfCols);
         rvMoviesList.setLayoutManager(layoutManager);
 
@@ -233,4 +235,6 @@ public class MainActivity extends AppCompatActivity implements MovieAdapter.Movi
         intentToStartMovieDetailActivity.putExtra(Intent.EXTRA_TEXT, movieInString);
         startActivity(intentToStartMovieDetailActivity);
     }
+
+
 }
--- app/src/main/java/com/example/android/popularmovies/VarColumnGridLayoutManager.java
+++ app/src/main/java/com/example/android/popularmovies/VarColumnGridLayoutManager.java
@@ -4,53 +4,92 @@ import android.content.Context;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.TypedValue;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by vincent on 7/7/17.
  */
 
 public class VarColumnGridLayoutManager extends GridLayoutManager {
-    private int mColumnWidth;
-    private boolean mColumnWidthChanged = true;
 
-    public VarColumnGridLayoutManager(Context context, int columnWidth) {
-        super(context, 1);
-        setColumnWidth(checkedColumnWidth(context, columnWidth));
+    private int spanCount = 1;
+
+    public VarColumnGridLayoutManager(Context context, int spanCount) {
+        super(context, spanCount, VERTICAL, false);
+
+        this.spanCount = spanCount;
     }
-    public VarColumnGridLayoutManager(Context context, int columnWidth, int orientation, boolean reverseLayout) {
-        super(context, 1, orientation, reverseLayout);
-        setColumnWidth(checkedColumnWidth(context, columnWidth));
+
+    public VarColumnGridLayoutManager(Context context, int spanCount, int orientation, boolean reverseLayout) {
+        super(context, spanCount, orientation, reverseLayout);
     }
+
+    private int[] mMeasuredDimension = new int[2];
+
     @Override
-    public void onLayoutChildren(RecyclerView.Recycler recycler, RecyclerView.State state) {
-        int width = getWidth();
-        int height = getHeight();
-        if (mColumnWidthChanged && mColumnWidth > 0 && width > 0 && height > 0) {
-           int totalSpace;
+    public void onMeasure(RecyclerView.Recycler recycler, RecyclerView.State state,
+                          int widthSpec, int heightSpec) {
+        final int widthMode = View.MeasureSpec.getMode(widthSpec);
+        final int heightMode = View.MeasureSpec.getMode(heightSpec);
+        final int widthSize = View.MeasureSpec.getSize(widthSpec);
+        final int heightSize = View.MeasureSpec.getSize(heightSpec);
+        int width = 0;
+        int height = 0;
+        for (int i = 0; i < ((double) getItemCount() / (double) spanCount); i++) {
+            if (getOrientation() == HORIZONTAL) {
 
-            if (getOrientation() == VERTICAL) {
-                totalSpace = width - getPaddingRight() - getPaddingLeft();
+                measureScrapChild(recycler, i,
+                        View.MeasureSpec.makeMeasureSpec(i, View.MeasureSpec.UNSPECIFIED),
+                        heightSpec,
+                        mMeasuredDimension);
+
+                width = width + mMeasuredDimension[0];
+                if (i == 0) {
+                    height = mMeasuredDimension[1];
+                }
+            } else {
+                measureScrapChild(recycler, i,
+                        widthSpec,
+                        View.MeasureSpec.makeMeasureSpec(i, View.MeasureSpec.UNSPECIFIED),
+                        mMeasuredDimension);
+                height = height + mMeasuredDimension[1];
+                if (i == 0) {
+                    width = mMeasuredDimension[0];
+                }
             }
-            else {
-                totalSpace = height - getPaddingTop() - getPaddingBottom();
-            }
-            int spanCount = Math.max(1, totalSpace / mColumnWidth);
-            setSpanCount(spanCount);
-            mColumnWidthChanged = false;
         }
-        super.onLayoutChildren(recycler, state);
-    }
-    private int checkedColumnWidth(Context context, int columnWidth) {
-        if (columnWidth <= 0) {
-            columnWidth = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 48,
-                    context.getResources().getDisplayMetrics());
+        switch (widthMode) {
+            case View.MeasureSpec.EXACTLY:
+                width = widthSize;
+            case View.MeasureSpec.AT_MOST:
+            case View.MeasureSpec.UNSPECIFIED:
         }
-        return columnWidth;
+
+        switch (heightMode) {
+            case View.MeasureSpec.EXACTLY:
+                height = heightSize;
+            case View.MeasureSpec.AT_MOST:
+            case View.MeasureSpec.UNSPECIFIED:
+        }
+
+        setMeasuredDimension(width, height);
     }
-    public void setColumnWidth(int newColumnWidth) {
-        if (newColumnWidth > 0 && newColumnWidth != mColumnWidth) {
-            mColumnWidth = newColumnWidth;
-            mColumnWidthChanged = true;
+
+    private void measureScrapChild(RecyclerView.Recycler recycler, int position, int widthSpec,
+                                   int heightSpec, int[] measuredDimension) {
+        View view = recycler.getViewForPosition(position);
+        recycler.bindViewToPosition(view, position);
+        if (view != null) {
+            RecyclerView.LayoutParams p = (RecyclerView.LayoutParams) view.getLayoutParams();
+            int childWidthSpec = ViewGroup.getChildMeasureSpec(widthSpec,
+                    getPaddingLeft() + getPaddingRight(), p.width);
+            int childHeightSpec = ViewGroup.getChildMeasureSpec(heightSpec,
+                    getPaddingTop() + getPaddingBottom(), p.height);
+            view.measure(childWidthSpec, childHeightSpec);
+            measuredDimension[0] = view.getMeasuredWidth() + p.leftMargin + p.rightMargin;
+            measuredDimension[1] = view.getMeasuredHeight() + p.bottomMargin + p.topMargin;
+            recycler.recycleView(view);
         }
     }
 
